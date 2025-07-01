95985c950ac2c7ec7299f976e15b10943fc31457
jintao zhang <44070470+jiz17043@users.noreply.github.com>
2025-06-30 21:52:31 -0400

    test again

--- test/flaky.test.js
+++ test/flaky.test.js
@@ -3,3 +3,12 @@ const { sometimesFails } = require('../src/funct');
 test('flaky test: sometimesFails returns true', () => {
   expect(sometimesFails()).toBe(true);
 });
+
+test('sometimesFails returns a boolean', () => {
+  const result = sometimesFails();
+  expect(typeof result).toBe('boolean');
+});
+
+test('sometimesFails does not throw', () => {
+  expect(() => sometimesFails()).not.toThrow();
+});
